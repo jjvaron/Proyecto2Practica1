a7f668df205a79cde9f5f2b737e76425c0ac914c
jjvaron <47426707+jjvaron@users.noreply.github.com>
2020-09-11 20:40:39 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,18 +8,22 @@ int8_t temperatura;//ya va a estar en celsius
 int8_t tempUnidades;//vamos a guardar el BCD de unidades
 int8_t tempDecenas;//vamos a guardar el BCD de decenas
 
-Tm_Periodico sondeoADC,sondeoDisplay;
+Tm_Periodico sondeoADC,sondeoDisplay,baseTiempo;
 Tm_Inicie_periodico (&sondeoADC,TIEMPOADC);// iniciar periodico de ADC
 Tm_Inicie_periodico (&sondeoDisplay,TIEMPODISPLAY);// iniciar periodico de Display
+Tm_Inicie_periodico (&baseTiempo,TIEMPOBASE);// iniciar periodico de Display
 
 
 void main (void)
 {
-    Tm_Procese_tiempo (&sondeoADC);
-    Tm_Procese_tiempo (&sondeoDisplay);
-
-}
-
+for(;;)
+{
+	if(m_Hubo_periodico (&baseTiempo))
+	{
+	    Tm_Baje_periodico (&&baseTiempo);//reset de condicion baseTiempo
+	    Tm_Procese_tiempo (&sondeoADC);
+	    Tm_Procese_tiempo (&sondeoDisplay);
+	}
 if(Tm_Hubo_periodico (&sondeoADC))// condicion de ADC
         {
             Tm_Baje_periodico (&sondeoADC);//reset de condicion ADC
@@ -31,3 +35,5 @@ if(Tm_Hubo_periodico (&sondeoADC))// condicion de ADC
     { Tm_Baje_periodico (&sondeoDisplay);//reset de condicion
             DyC_Procese_Display(&tempUnidades,&tempDecenas);
         }
+} /*for*/
+} /*main*/
