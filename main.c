c7404933aed77f1c712ef5cf24a38f0987f7d4a5
jjvaron <47426707+jjvaron@users.noreply.github.com>
2020-09-12 07:55:16 -0500

    solución de no definición de int8_t
    hacia falta agregar la libreria  "display.h"

--- main.c
+++ main.c
@@ -11,9 +11,10 @@
 
 #include "nuestrostimers.h"
 #include "definiciones_y_configuraciones.h"
-
+#include "display.h"
+	
 // En nuestra implementacion esta deberia ser un global
-// si vamos a trabajar por interrupción o deberia estar en el
+// si vamos a trabajar por interrupciÃ³n o deberia estar en el
 // espacio de memoria del main.
 int8_t temperatura;//ya va a estar en celsius
 int8_t tempUnidades;//vamos a guardar el BCD de unidades
