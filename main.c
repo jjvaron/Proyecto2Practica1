4e8541e603c5bdb2224993c667f3f9524ab7d0ca
jjvaron <47426707+jjvaron@users.noreply.github.com>
2020-09-11 23:46:05 -0500

    actualización main.c
    se mejora la identación, se agrega estructura de tiempo (1ms) conocida como baseTiempo para entrar a la condición y procesar los tiempos de ADC y Display

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <avr/io.h>
+
+
 #include "nuestrostimers.h"
 #include "definiciones_y_configuraciones.h"
 
@@ -8,32 +11,33 @@ int8_t temperatura;//ya va a estar en celsius
 int8_t tempUnidades;//vamos a guardar el BCD de unidades
 int8_t tempDecenas;//vamos a guardar el BCD de decenas
 
-Tm_Periodico sondeoADC,sondeoDisplay,baseTiempo;
+Tm_Periodico sondeoADC,sondeoDisplay;
 Tm_Inicie_periodico (&sondeoADC,TIEMPOADC);// iniciar periodico de ADC
 Tm_Inicie_periodico (&sondeoDisplay,TIEMPODISPLAY);// iniciar periodico de Display
-Tm_Inicie_periodico (&baseTiempo,TIEMPOBASE);// iniciar periodico de Display
 
 
 void main (void)
 {
-for(;;)
-{
-	if(m_Hubo_periodico (&baseTiempo))
+	for(;;)
 	{
-	    Tm_Baje_periodico (&&baseTiempo);//reset de condicion baseTiempo
-	    Tm_Procese_tiempo (&sondeoADC);
-	    Tm_Procese_tiempo (&sondeoDisplay);
-	}
-if(Tm_Hubo_periodico (&sondeoADC))// condicion de ADC
-        {
-            Tm_Baje_periodico (&sondeoADC);//reset de condicion ADC
-	    DyC_Procese_ADC(&temperatura,&tempUnidades,&tempDecenas);
-        }
+		if(Tm_Hubo_periodico (&baseTiempo))
+		{
+			Tm_Baje_periodico (&&baseTiempo);//reset de condicion baseTiempo
+			Tm_Procese_tiempo (&sondeoADC);
+			Tm_Procese_tiempo (&sondeoDisplay);
+		}
+		if(Tm_Hubo_periodico (&sondeoADC))// condicion de ADC
+		{
+			Tm_Baje_periodico (&sondeoADC);//reset de condicion ADC
+			DyC_Procese_ADC(&temperatura,&tempUnidades,&tempDecenas);
+		}
 
-  if(Tm_Hubo_periodico (&sondeoDisplay))// condicion de Display entra cada
-                //TIEMPODISPLAY 10 milisegundos
-    { Tm_Baje_periodico (&sondeoDisplay);//reset de condicion
-            DyC_Procese_Display(&tempUnidades,&tempDecenas);
-        }
-} /*for*/
+		if(Tm_Hubo_periodico (&sondeoDisplay))// condicion de Display entra cada TIEMPODISPLAY 10 milisegundos
+		
+		{ 
+			Tm_Baje_periodico (&sondeoDisplay);//reset de condicion
+			DyC_Procese_Display(&tempUnidades,&tempDecenas);
+		}
+	} /*for*/
 } /*main*/
+
