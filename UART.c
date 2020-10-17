df26ecfc5c7c46971743785a02cf21d7ca20b5a9
SebastianParrado <47955595+SebastianParrado@users.noreply.github.com>
2020-10-17 09:54:48 -0500

    Add files via upload
    Códigos finales para la atención de eventos por Polling.
    Estos códigos están comentados. Versión 2.5.3

---
+++ UART.c
@@ -0,0 +1,149 @@
+/* Ruler 1         2         3         4         5         6         7        */
+/*
+ * Libreria para la comunicacion serial asincrona entre el microcontrolador y 
+ * el computador con una velocidad de 9600 baudios
+ */
+#include "UART.h"
+
+/* UART */
+void UART_init()
+{
+	DDRD |= (1<<1);							//PD1	COMO SALIDA TXa
+	DDRD &= ~(1<<0);						//PD0	COMO ENTRADA RX
+	UCSR0A = (0<<TXC0)|(0<<U2X0)|(0<<MPCM0);
+	UCSR0B = (0<<RXCIE0)|(0<<TXCIE0)|(0<<UDRIE0)|(1<<RXEN0)|(1<<TXEN0)|(0<<UCSZ02)|(0<<TXB80);
+	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)|(0<<UPM01)|(0<<UPM00)|(0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00)|(0<<UCPOL0);
+	UBRR0 = 103;								//NO DUPLICA VELOCIDAD 9600B A 160000
+}
+
+void UART_write(unsigned char caracter){
+	while(!(UCSR0A&(1<<5)));    // mientras el registro UDR0 esté lleno espera
+		UDR0 = caracter;            //cuando el el registro UDR0 está vacio se envia el caracter
+}
+
+// Funcion para la recepcion de caracteres
+void UART_read(UART_T *comunicacion)
+{
+	comunicacion->bandera |= 0x01;
+	comunicacion->dato_serial = UDR0;
+	while (!(UCSR0A & (1<<UDRE0)));
+	UDR0 = comunicacion->dato_serial;
+}
+
+char Hubo_RX(UART_T *comunicacion)
+{
+	return (comunicacion->bandera & 0x01);
+}
+
+void Baje_RX(UART_T *comunicacion)
+{
+	comunicacion->bandera &=~ 0x01;
+}
+
+void UART_write_txt(char* cadena) //cadena de caracteres de tipo char
+{			
+	while(*cadena !=0x00) //mientras el último valor de la cadena sea diferente a el caracter nulo
+	{				
+		UART_write(*cadena);			//transmite los caracteres de cadena
+		cadena++;						//incrementa la ubicación de los caracteres en cadena
+		//para enviar el siguiente caracter de cadena
+	}
+}
+
+// Funcion para la transmision del digito de decenas
+void UART_write_DEC(uint8_t *temperatura, char *dato_tx)
+{
+    uint8_t decenas;
+	decenas  = *temperatura/10;
+	*dato_tx = decenas + '0';   
+
+	while (!(UCSR0A & (1<<UDRE0)));
+	UDR0 = *dato_tx;
+}
+
+// Funcion para la transmision del digito de unidades
+void UART_write_UNI(uint8_t *temperatura, char *dato_tx)
+{
+	uint8_t unidades;
+	unidades = *temperatura%10;
+	*dato_tx = unidades + '0';
+	while (!(UCSR0A & (1<<UDRE0)));
+	UDR0 = *dato_tx;
+}
+
+void Inicie_estado_write(UART_T *comunicacion)
+{
+	comunicacion->fsm = s0;
+}
+
+void FSM_UART(UART_T *ptr_char)
+{
+	switch (ptr_char->fsm)
+	{
+		case s0:
+		if (ptr_char->dato_serial==INICIO)
+		{
+			ptr_char->fsm= s1;
+		}
+		else
+		{
+			ptr_char->fsm = s0;	
+		} /* endif */
+		break;
+
+		case s1:
+		if (ptr_char->dato_serial==DORMIR)
+		{	
+			ptr_char->fsm = s0;
+			
+			DyC_DeshabiliteADC(); //ADCSRA &=~ (1<<ADEN);
+			// Todos los pines se apagan
+			PORTB = 0x00;
+			PORTC = 0x00;
+			PORTD = 0x00;
+			
+			// Modo Idle
+			/*
+			SMCR &=~ (1<<SM2);
+			SMCR &=~ (1<<SM1);
+			SMCR &=~ (1<<SM0);
+			*/
+			// Ajusta el registro para ponerlo en modo Power-Down/Baja Potencia
+			SMCR &=~ (1<<SM2);
+			SMCR |=  (1<<SM1);
+			SMCR &=~ (1<<SM0);
+			// Ajusta el registro en modo de Power-save/Ahorro de potencia
+			/*
+			SMCR &=~ (1<<SM2);
+			SMCR |=  (1<<SM1);
+			SMCR |=  (1<<SM0);
+			*/
+			// Modo Standby
+			/*
+			SMCR |=  (1<<SM2);
+			SMCR |=  (1<<SM1);
+			SMCR &=~ (1<<SM0);
+			*/
+			// Modo Extended Standby
+			/*
+			SMCR |= (1<<SM2);
+			SMCR |= (1<<SM1);
+			SMCR |= (1<<SM0);
+			*/
+			SMCR |= (1<<SE);
+			__asm__ __volatile__ ( "sleep" );
+		}
+		else
+		{
+			ptr_char->fsm = s0;
+		}
+		break;
+	} /* endif */
+} /* FSM_UART */
+
+void ConfigurarModo()
+{
+	SMCR = 0x00;
+	EIMSK |= (1<<INT0); //pin 4 INT0
+	EICRA |= (1 << ISC01) || ( 1 << ISC00 ); // Rising edge on both
+}
---
+++ display.c
@@ -0,0 +1,137 @@
+/* Ruler 1         2         3         4         5         6         7        */
+/*
+ * Declaración de funciones previamente enunciadas en la libreria de
+ * definiciones y configuraciones
+ */
+
+ /* ********************************* Headers ******************************** */
+
+ /* --------------------- Inclusion de librerias estandar -------------------- */
+ #include "display.h"
+ 
+ /* ******************************** Funciones ******************************* */
+ 
+ /* ----------------------------- Manejo Display  ---------------------------- */
+ // Separa las unidades y decenas del valor de temperatura
+void D_inicie_display (D_Display *disp, uint8_t *temperatura)
+{
+	disp->tempUnidades = *temperatura%10;
+    disp->tempDecenas  = *temperatura/10;
+}
+   
+// Apaga el display antes de cambiar de numero
+void D_apague_display() 
+{
+	// baja todos los pines definidos en los puertos
+	PORTB &= 0x00;
+	PORTC &= 0x00; 
+	PORTD &= 0x00; 
+}
+// Esto es para definir el estado inicial
+void D_Inicie_Estado(D_Display *disp)
+{
+	disp->mostrando    = UNIDADES;
+}
+
+/* -------------------------------- Display --------------------------------- */
+// Mostrar los numeros en el 7-seg
+void D_Procese_display (D_Display *disp) //, int8_t valor)
+{
+	switch(disp->mostrando)
+	{
+		case UNIDADES: //mostramos unidades
+			D_apague_display();
+		    // Puerto en 1 desde protoboard
+		    PORTD |= TRANDISPUNIDADES;
+		    PORTC &=~ TRANDISPDECENAS;
+		    PORTB |= num2BCDPUERTOB(disp->tempUnidades);
+		    PORTC |= num2BCDPUERTOC(disp->tempUnidades);
+		    PORTD |= num2BCDPUERTOD(disp->tempUnidades);
+		  
+            disp->mostrando=DECENAS;// cambiar estado
+        break;
+	  
+        case DECENAS: //mostrar decenas
+			D_apague_display();
+			// Puerto en 1 desde protoboard
+			PORTD &=~ TRANDISPUNIDADES; //0b00010000;
+			PORTC |= TRANDISPDECENAS;
+			PORTB |= num2BCDPUERTOB(disp->tempDecenas);
+			PORTC |= num2BCDPUERTOC(disp->tempDecenas);
+			PORTD |= num2BCDPUERTOD(disp->tempDecenas);
+			
+            disp->mostrando=UNIDADES;// cambiar estado
+	    break;
+	  
+        default:
+            disp->mostrando=DECENAS;
+	}
+}
+ 
+/* -----------------------------  Look-up tables ----------------------------- */
+
+uint8_t num2BCDPUERTOB(uint8_t num)
+{
+	//PORB SEG                  XXXXXFAB
+	//PORB PIN                  76543210
+	uint8_t tablaPuertoB[12] = {0b00000111, // 0
+							   0b00000001,  // 1
+							   0b00000011,  // 2
+							   0b00000011,  // 3
+							   0b00000101,  // 4
+							   0b00000110,  // 5
+							   0b00000110,  // 6
+							   0b00000011,  // 7
+							   0b00000111,  // 8
+							   0b00000111,  // 9
+							   0b00000000,  // Signo menos -
+							   0b00000110}; // E de error 
+	  
+   if(num>=12||num<0)
+        return tablaPuertoB[11];/*en la pos 11 de la tabla esta el error*/
+    return tablaPuertoB[num];
+ }
+ 
+ uint8_t num2BCDPUERTOC(uint8_t num)
+ {
+	 //PORC PIN                 XXXXXX+G // +=DECENAS
+	 //PORC PIN                 76543210
+	 uint8_t tablaPuertoC[12] = {0b00000000, // 0
+	  						    0b00000000,  // 1
+							    0b00000001,  // 2
+							    0b00000001,  // 3
+							    0b00000001,  // 4
+							    0b00000001,  // 5
+							    0b00000001,  // 6
+							    0b00000000,  // 7
+							    0b00000001,  // 8
+							    0b00000001,  // 9
+							    0b00000001,  // -
+							    0b00000001}; // E
+	  
+	if(num>=12||num<0)
+		return tablaPuertoC[11];/*en la pos 11 de la tabla esta el error*/
+	return tablaPuertoC[num];
+ }
+ 
+uint8_t num2BCDPUERTOD(uint8_t num)
+{
+	//PORD PIN                CDE-XXXX // -=UNIDADES
+	//PORD PIN                76543210
+	uint8_t tablaPuertoD[12] = {0b11100000, // 0
+						    0b10000000,  // 1
+							0b01100000,  // 2
+							0b11000000,  // 3
+							0b10000000,  // 4
+							0b11000000,  // 5
+							0b11100000,  // 6
+							0b10000000,  // 7
+							0b11100000,  // 8
+							0b11000000,  // 9
+							0b00000000,  // -
+							0b01100000}; // E
+	   
+	if(num>=12||num<0)
+	return tablaPuertoD[11];/*en la pos 11 de la tabla esta el error*/
+	return tablaPuertoD[num];
+}
